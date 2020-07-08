a30085adb86d74fdb3e2689568c9afbcb27b5c47
kosuke <k.shibata9447@gmail.com>
2020-07-08 19:11:40 +0900

    add columns to DB
    UserモデルのDBに名前、郵便番号、住所、自己紹介文のカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal, :address, :introduction])
+    end
 end
