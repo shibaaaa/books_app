88422d8b36f30c1f0fe61e15c4b44a7b05f1aed7
kosuke <k.shibata9447@gmail.com>
2020-07-08 14:55:44 +0900

    set up device
    User modelをセットアップした。
    deviceのviewsをgenerateした。

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_115306) do
+ActiveRecord::Schema.define(version: 2020_07_08_032146) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -20,4 +19,17 @@ ActiveRecord::Schema.define(version: 2020_07_02_115306) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
