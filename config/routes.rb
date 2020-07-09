a0da0c8dbbace67322ac1298e8449fd1369241ec
kosuke <k.shibata9447@gmail.com>
2020-07-09 18:25:15 +0900

    generate profile page
    プロフィールページを作成し、日本語対応した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  get "users/show"
+  devise_for :users, controllers: {
+        registrations: "users/registrations"
+  }
   resources :books
+  resources :users, only: [:show]
   root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
