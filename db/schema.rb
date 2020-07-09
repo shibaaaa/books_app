3f6a50d23b708cf5b910a0b2825402b6bb092e68
kosuke <k.shibata9447@gmail.com>
2020-07-09 23:12:03 +0900

    delete default value from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_093418) do
+ActiveRecord::Schema.define(version: 2020_07_09_130015) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2020_07_08_093418) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
