56136d10fee0caa0e56cd0a565ace298f1301c09
kosuke <k.shibata9447@gmail.com>
2020-07-13 22:36:18 +0900

    add columns to users

---
+++ db/migrate/20200713115910_add_omniauth_to_users.rb
@@ -0,0 +1,8 @@
+class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :provider, :string, null: false, default: ""
+    add_column :users, :uid, :string, null: false, default: ""
+
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
