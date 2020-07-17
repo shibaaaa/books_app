50886c09d12533ddaef23e35710d5c918d1bbaac
kosuke <k.shibata9447@gmail.com>
2020-07-17 11:07:41 +0900

    Implemented image uploading function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal, :address, :introduction])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal, :address, :introduction])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal, :address, :introduction, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal, :address, :introduction, :avatar])
     end
 end
