3d6bb0385878affe9352a7f820aa30fe02db7b2f
kosuke <k.shibata9447@gmail.com>
2020-07-25 22:49:03 +0900

    Associated of User model and Follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,20 @@
 
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  has_many :active_follows, class_name: "Follow",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :passive_follows, class_name: "Follow",
+            foreign_key: "followed_id",
+            dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+
   has_one_attached :avatar
+
   devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+          :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
   def self.find_for_github_oauth(auth, signed_in_resource = nil)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
