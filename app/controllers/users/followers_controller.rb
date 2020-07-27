ad228f518c651a4aaf067c7b12be8da2cd0509f1
kosuke <k.shibata9447@gmail.com>
2020-07-27 23:05:05 +0900

    Delete "before_action" from FollowsController
    Because it is described ApplicationController

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::FollowersController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-
   def index
     @user = User.find(params[:user_id])
     @users = @user.followers
