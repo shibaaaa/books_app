63f13f8c65e3e02759f519e64a3b982e85b197fb
shibaaa <k.shibata9447@gmail.com>
2020-09-14 14:48:48 +0900

    Fix by rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_29_114311) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
